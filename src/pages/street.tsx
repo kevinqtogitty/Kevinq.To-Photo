c0438a0d1979595115a5a82f3e04d02bc4d3533b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-22 22:07:43 +0200

    removed onload react-spring animations

--- src/pages/street.tsx
+++ src/pages/street.tsx
@@ -1,35 +1,14 @@
 import type React from 'react';
 import json from '../../db.json';
 import { ImageContainer, Photo } from '../stylesheets/photo_container_styles';
-import { useTrail } from 'react-spring';
-import { useEffect, useState } from 'react';
 
 const Street = () => {
-  const [active, setActive] = useState(false);
-  const imgUrls: string[] = json.photoGroups.street.map((item) => item.imgUrl);
-  const config = {
-    tension: 280,
-    friction: 60
-  };
-
-  useEffect(() => {
-    setActive(true);
-  }, []);
-
-  const trails = useTrail(imgUrls.length, {
-    opacity: active ? 1 : 0,
-    config
-  });
+  const data: string[] = json.photoGroups.street.map((item) => item.imgUrl);
 
   return (
     <ImageContainer className="streetPhotos" style={{ zIndex: -1 }}>
-      {trails.map((animation, i) => (
-        <Photo
-          className="streetImage"
-          src={imgUrls[i]}
-          key={i}
-          style={animation}
-        />
+      {data.map((url, i) => (
+        <Photo className="belowImage" src={url} key={i} />
       ))}
     </ImageContainer>
   );
