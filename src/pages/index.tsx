c891df5cb244cdb6b4d8ee3387bfec26850ad2d3
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-22 22:08:04 +0200

    removed onload react-spring animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,14 @@
 import type React from 'react';
 import json from '../../db.json';
 import { ImageContainer, Photo } from '../stylesheets/photo_container_styles';
-import { useTrail } from 'react-spring';
-import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const [active, setActive] = useState(false);
-  const imgUrls = json.photoGroups.homepage.map((item) => item.imgUrl);
-  const config = {
-    tension: 280,
-    friction: 60
-  };
-
-  useEffect(() => {
-    setActive(true);
-  }, []);
-
-  const trails = useTrail(imgUrls.length, {
-    opacity: active ? 1 : 0,
-    transform: active ? 'translateY(0%)' : 'translateY(0%)',
-    config
-  });
+  const data = json.photoGroups.homepage.map((item) => item.imgUrl);
 
   return (
     <ImageContainer className="homepage">
-      {trails.map((animation, i) => (
-        <Photo src={imgUrls[i]} key={i} style={animation} />
+      {data.map((url, i) => (
+        <Photo className="belowImage" src={url} key={i} />
       ))}
     </ImageContainer>
   );
