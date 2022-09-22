68489a4754a2fd0a0f8480470ffd11966da9e7b0
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-22 20:32:14 +0200

    Initial Commit

---
+++ src/pages/store.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainWrapper } from '../stylesheets/form_styles';
+
+const Store = () => {
+  return (
+    <MainWrapper>
+      <>Under construction!</>
+    </MainWrapper>
+  );
+};
+
+export default Store;
