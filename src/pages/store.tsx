53b0fdcb492383b3e6c20131f7a33716a5940c85
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-25 17:59:43 +0200

    Print store is now live and able to accept payments via stripe!

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MainWrapper } from '../stylesheets/form_styles';
+import AvailableProducts from '../components/AvailableProducts';
 
 const Store = () => {
   return (
     <MainWrapper>
-      <>Under construction!</>
+      <AvailableProducts />
     </MainWrapper>
   );
 };
