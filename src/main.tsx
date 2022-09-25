95682fa35ffd76bb358e24782c60c691069a21c8
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-25 18:04:31 +0200

    Added redux store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './stylesheets/globalStyle.css';
+import { store } from './redux_store/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
