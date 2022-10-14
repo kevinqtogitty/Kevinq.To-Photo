613f567ae08cc3d39ed3062dc1567853d18b74d4
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-14 12:42:15 +0200

    removed unused imports

--- src/pages/onTheRoad.tsx
+++ src/pages/onTheRoad.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import json from '../../db.json';
 import { Photo } from '../stylesheets/styled_components/component_styles/photoStyles';
 import {
